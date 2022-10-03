06199114604e37c63fd8af35cdd451a204d8e513
Paul Thomas <paulthomas2107@gmail.com>
2022-10-03 19:11:27 +0100

    More

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 const isMac = process.platform === 'darwin';
 const isDev = process.env.NODE_ENV !== 'production';
 
+// Create the main window
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: 'Image Reszier',
@@ -18,14 +19,50 @@ function createMainWindow() {
   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
+// Create about windown
+function createAboutWindow() {
+  const aboutWindow = new BrowserWindow({
+    title: 'About Image Reszier',
+    width: 300,
+    height: 300,
+  });
+
+  aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'));
+}
+
+// App is ready
 app.whenReady().then(() => {
   createMainWindow();
+  // Implement Menu
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createMainWindow();
     }
   });
 });
+
+// Menu template
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: 'fileMenu',
+  },
+];
 
 app.on('window-all-closed', () => {
   if (!isMac) {
